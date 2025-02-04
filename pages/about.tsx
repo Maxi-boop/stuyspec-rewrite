e1fc68652878ddfb355fc23f424259ab1cb4474a
Maxiboops <maximilianopettica@gmail.com>
2025-02-03 22:53:56 -0500

    Primary fixes on staff.tsx, and CSS pages for the former. Additionally, the about page was updated and a modal and a card module were added for conviencence of the staff page! Some photos were temporary placed in the pages>about section. Those will be moved shortly

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,68 +1,150 @@
 import Head from "next/head";
 import styles from "../styles/AboutItemGrid.module.css";
+import stuyvesantSpectatorLogo from "./about/Untitled_design-removebg-preview (1).png";
 import Image from "next/image";
 import one_hundred_years_logo from "../public/images/100years_logo.png";
 import Link from "next/link";
 
 function AboutUs(props: any) {
-	const pages: Array<{
-		name: string;
-		link: string;
-		summary: string;
-		image: string;
-	}> = [
-			{
-				name: "Our Charter",
-				link: "/our-charter",
-				summary:
-					"Curious about our policies? Learn more by reading the charter of the Stuyvesant Spectator.",
-				image: "string",
-			},
-			{
-				name: "Advertise",
-				link: "/advertise",
-				summary:
-					"Considering posting an advertisement? Learn more about how you can advertise on The Stuyvesant Spectator's website.",
-				image: "string",
-			},
-			{
-				name: "Sponsors",
-				link: "/sponsors",
-				summary:
-					"Considering sponsoring us? Learn more about how you can sponsor us here.",
-				image: "string",
-			},
-			{
-				name: "Staff",
-				link: "/staff",
-				summary: "Learn more about our current or past staff members.",
-				image: "string",
-			},
-			{
-				name: "Developers",
-				link: "/developers",
-				summary:
-					"Learn more about the developers behind The Stuyvesant Spectator's website.",
-				image: "string",
-			},
-			{
-				name: "Contact",
-				link: "/contact",
-				summary: "Want to contact us? Get in touch.",
-				image: "string",
-			},
-		];
+  const pages: Array<{
+    name: string;
+    link: string;
+    summary: string;
+    image: string;
+  }> = [
+    {
+      name: "Our Charter",
+      link: "/our-charter",
+      summary:
+        "Curious about our policies? Learn more by reading the charter of the Stuyvesant Spectator.",
+      image: "string",
+    },
+    {
+      name: "Advertise",
+      link: "/advertise",
+      summary:
+        "Considering posting an advertisement? Learn more about how you can advertise on The Stuyvesant Spectator's website.",
+      image: "string",
+    },
+    {
+      name: "Sponsors",
+      link: "/sponsors",
+      summary:
+        "Considering sponsoring us? Learn more about how you can sponsor us here.",
+      image: "string",
+    },
+    {
+      name: "Staff",
+      link: "/staff",
+      summary: "Learn more about our current or past staff members.",
+      image: "string",
+    },
+    {
+      name: "Developers",
+      link: "/developers",
+      summary:
+        "Learn more about the developers behind The Stuyvesant Spectator's website.",
+      image: "string",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      summary: "Want to contact us? Get in touch.",
+      image: "string",
+    },
+  ];
 
-	return (
-		<div>
-			<Head>
-				<title>About Us</title>
-				<meta name="description" content="The Stuyvesant Spectator" />
-			</Head>
+  return (
+    <div>
+      <Head>
+        <title>About Us</title>
+        <meta name="description" content="The Stuyvesant Spectator" />
+      </Head>
+      <main>
+        <div className={styles.aboutUsContainer}>
+          <div className={styles.aboutUsText}>
+            <h1 id={styles.aboutUsHeader}>The Spectator</h1>
+            <h1 id={styles.mainCatchPhrase}>Interesting Quote Goes Here</h1>
+            <p id={styles.aboutUsDescription}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              dignissim enim sit amet mollis auctor. Suspendisse suscipit tempus
+              velit ut eleifend. Donec egestas odio lacus. Proin ipsum dolor,
+              sodales vel lacinia ac, bibendum et odio. Etiam quis massa a
+              mauris condimentum commodo. Nunc condimentum nulla leo, vitae
+              aliquam lectus efficitur ac. Ut sem orci, mollis vitae feugiat eu,
+              placerat in est.
+            </p>
+          </div>
+          <div className={styles.aboutUsImage}>
+            <div id={styles.logo}>
+              <img
+                src={stuyvesantSpectatorLogo.src}
+                alt="Stuyvesant Spectator Logo"
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.gridHeaderContainer}>
+          <div id={styles.line}></div>
+          <h1 id={styles.gridHeader}>About Us</h1>
+          <div id={styles.line}></div>
+        </div>
+        <div className={styles.grid}>
+          {pages.map((page, key) => (
+            <div id={styles.item} key={key}>
+              <div id={styles.overlay}>
+                <div className={styles.itemContent}>
+                  <div className={styles.itemText}>
+                    {/*<Link passHref href={`/about${page.link}`}>
+							<>
+							 <Image
+								id={styles.image}
+								alt="The Stuyvesant Spectator: 100 years of journalism"
+								src={one_hundred_years_logo}
+							/> */}
+                    <div id={styles.header}>
+                      <div id={styles.headerName}>
+                        <h2>{page.name}</h2>
+                      </div>
+                    </div>
+                    {/* </>
+						</Link> */}
+                  </div>
+                  <div className={styles.expandItemContainer}>
+                    <div id={styles.headerSummary}>
+                      <p>{page.summary}</p>
+                    </div>
 
+                    <Link passHref href={`/about${page.link}`}>
+                      <button id={styles.expandItem}>
+                        <p id={styles.expandItemPlus}>+</p>
+                        <p id={styles.expandItemLearnMore}>
+                          <span>L</span>
+                          <span>e</span>
+                          <span>a</span>
+                          <span>r</span>
+                          <span>n</span>
+                          <span id={styles.expandItemSpace}> </span>
+                          <span>M</span>
+                          <span>o</span>
+                          <span>r</span>
+                          <span>e</span>
+                        </p>
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+    /** 
 			<main id={styles.container}>
 				<h1 id={styles.textCentered}>About Us</h1>
 				<div id={styles.grid}>
+			
 					{pages.map((page, key) => (
 						<div id={styles.item} key={key}>
 							<Link passHref href={`/about${page.link}`}>
@@ -78,10 +160,12 @@ function AboutUs(props: any) {
 							</Link>
 						</div>
 					))}
+
+				
 				</div>
 			</main>
-		</div>
-	);
-};
+**/
+  );
+}
 
 export default AboutUs;
--- pages/about/staff.tsx
+++ pages/about/staff.tsx
@@ -1,128 +1,556 @@
 import Head from "next/head";
 import styles from "../../styles/Staff.module.css";
 import { generateMetaTags } from "../../utils/generateMetaTags";
-
+import { StaticImageData } from "next/image";
+import { useEffect, useRef, useState } from "react";
+import Modal from "./staff/modal";
+import Card from "./staff/cards";
+import DepartmentTags from "./staff/tags/departmentTags";
 function StaffPage() {
-	const page_title = "Staff - The Stuyvesant Spectator";
-	const meta_url = `https://stuyspec.com/about/staff`;
-	const meta_description = `The members of The Stuyvesant Spectator's 2024-2025 Editorial Board.`;
-
-	return (
-		<>
-			<Head>
-				{generateMetaTags(page_title, meta_description, meta_url)}
-			</Head>
-			<main id={styles.main}>
-				<h1>Members of the 2023-2024 Spectator Editorial Board</h1>
-				<section id={styles.departments}>
-					<p className={styles.special_def}>* Managing Board</p>
-					<p className={styles.special_def}>** Editors-in-Training</p>
-
-					<div>
-						<h3 className={styles.EIC}>Editors In Chief</h3>
-						<p>Suyeon Ryu*</p>
-						<p>Khush Wadhwa*</p>
-					</div>
-					<div>
-						<h3 className={styles.news}>News Editors</h3>
-						<p>Talia Arcasoy*</p>
-						<p>Seth Fenton*</p>
-						<p>Zoey Marcus</p>
-						<p>Brendan Tan</p>
-						<p>Lauren Yang</p>
-					</div>
-					<div>
-						<h3 className={styles.features}>Features Editors</h3>
-						<p>Abigail Jin</p>
-						<p>Hifza Kaleem</p>
-						<p>Cathleen Xi</p>
-					</div>
-					<div>
-						<h3 className={styles.ops}>Opinions Editors</h3>
-						<p>Joanne Hwang</p>
-						<p>Helen Mancini*</p>
-						<p>Amaryllis Sun</p>
-						<p>Myles Vuong</p>
-					</div>
-					<div>
-						<h3 className={styles.science}>Science Editors</h3>
-						<p>Aarya Balakrishnan</p>
-						<p>Sonya Cisse</p>
-						<p>Ryan Lin</p>
-						<p>Michelle Ng</p>
-					</div>
-					<div>
-						<h3 className={styles.ae}>Arts & Entertainment Editors</h3>
-						<p>Benson Chen</p>
-						<p>Zoë Feigelson</p>
-						<p>Madeline Hutchinson</p>
-						<p>Emile Lee-Suk</p>
-						<p>Santino Suarez*</p>
-					</div>
-					<div>
-						<h3 className={styles.humor}>Humor Editors</h3>
-						<p>Michelle “Mike” Huang</p>
-						<p>Munem Tajwar</p>
-					</div>
-					<div>
-						<h3 className={styles.sports}>Sports Editors</h3>
-						<p>Elijah Choi</p>
-						<p>Leonardo Guidi</p>
-						<p>Kaileen So</p>
-						<p>Evan Wong</p>
-						<p>Duncan Park</p>
-					</div>
-					<div>
-						<h3 className={styles.photos}>Photography Editors</h3>
-						<p>Geoffrey Huang</p>
-						<p>Ibtida Khurshed</p>
-						<p>Honora Muratori</p>
-
-					</div>
-					<div>
-						<h3 className={styles.art}>Art Directors</h3>
-						<p>Jaden Bae</p>
-						<p>Stacey Chen</p>
-						<p>Rhea Malhotra</p>
-						<p>Chuer Zhong</p>
-					</div>
-					<div>
-						<h3 className={styles.layout}>Layout Editors</h3>
-						<p>Anjali Bechu**</p>
-						<p>Elaine Liu</p>
-						<p>Isabel Noh**</p>
-						<p>Andre Wang</p>
-						<p>Karen Xu</p>
-						<p>Jasper Yu-Dawidowicz</p>
-					</div>
-					<div>
-						<h3 className={styles.copy}>Copy Editors</h3>
-						<p>Yuna Lee</p>
-						<p>Ryan Park</p>
-						<p>Niamh Werner</p>
-					</div>
-					<div>
-						<h3 className={styles.biz}>Business Managers</h3>
-						<p>Fiona Cai</p>
-						<p>Christopher Louie</p>
-						<p>Anderson Oh</p>
-						<p>Everett Yu-Dawidowicz**</p>
-					</div>
-					<div>
-						<h3 className={styles.web}>Web Editors</h3>
-						<p>Adam Choi</p>
-						<p>Angela Mashuryan</p>
-						<p>Lenny Metlitsky*</p>
-						<p>Ankita Saha</p>
-					</div>
-					<div>
-						<h3 className={styles.faculty}>Faculty Advisor</h3>
-						<p>Kerry Garfinkel</p>
-					</div>
-				</section>
-			</main>
-		</>
-	);
-};
+  let department: number = 0;
+  const page_title = "Staff - The Stuyvesant Spectator";
+  const meta_url = `https://stuyspec.com/about/staff`;
+  const meta_description = `The members of The Stuyvesant Spectator's 2024-2025 Editorial Board.`;
+  interface membersInterface {
+    picture?: StaticImageData | undefined;
+    name: string;
+    managingBoard: boolean;
+    editorsInTraining: boolean;
+    departmentNumber: number;
+  }
+  const sectionalDepartment: Array<{
+    departmentTitle: string;
+    members: membersInterface[];
+  }> = [
+    {
+      departmentTitle: "Editor In Chief",
+      members: [
+        {
+          picture: undefined,
+          name: "Suyeon Ryu",
+          managingBoard: true,
+          editorsInTraining: false,
+          departmentNumber: 0,
+        },
+        {
+          picture: undefined,
+          name: "Khush Wadwa",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 0,
+        },
+      ],
+    },
+    {
+      departmentTitle: "News Editors",
+      members: [
+        {
+          picture: undefined,
+          name: "Talia Arcasoy",
+          managingBoard: true,
+          editorsInTraining: false,
+          departmentNumber: 1,
+        },
+        {
+          picture: undefined,
+          name: "Seth Fenton",
+          managingBoard: true,
+          editorsInTraining: false,
+          departmentNumber: 1,
+        },
+        {
+          picture: undefined,
+          name: "Zoey Marcus",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 1,
+        },
+        {
+          picture: undefined,
+          name: "Brendan Tan",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 1,
+        },
+        {
+          picture: undefined,
+          name: "Lauren Yang",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 1,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Features Editors",
+      members: [
+        {
+          picture: undefined,
+          name: "Abigail Jin",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 2,
+        },
+        {
+          picture: undefined,
+          name: "Hifza Kaleem",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 2,
+        },
+        {
+          picture: undefined,
+          name: "Cathleen Xi",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 2,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Opinions Editors",
+      members: [
+        {
+          picture: undefined,
+          name: "Joanne Hwang",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 3,
+        },
+        {
+          picture: undefined,
+          name: "Helen Mancini",
+          managingBoard: true,
+          editorsInTraining: false,
+          departmentNumber: 3,
+        },
+        {
+          picture: undefined,
+          name: "Amaryllis Sun",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 3,
+        },
+        {
+          picture: undefined,
+          name: "Myles Vuong",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 3,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Science Editors",
+      members: [
+        {
+          picture: undefined,
+          name: "Aarya Balakrishnan",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 4,
+        },
+        {
+          picture: undefined,
+          name: "Sonya Cisse",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 4,
+        },
+        {
+          picture: undefined,
+          name: "Ryan Lin",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 4,
+        },
+        {
+          picture: undefined,
+          name: "Michelle Ng",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 4,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Arts & Entertainment Editors",
+      members: [
+        {
+          picture: undefined,
+          name: "Benson Chen",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 5,
+        },
+        {
+          picture: undefined,
+          name: "Zoë Feigelson",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 5,
+        },
+        {
+          picture: undefined,
+          name: "Madeline Hutchinson",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 5,
+        },
+        {
+          picture: undefined,
+          name: "Emile Lee-Suk",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 5,
+        },
+        {
+          picture: undefined,
+          name: "Santio Suarez",
+          managingBoard: true,
+          editorsInTraining: false,
+          departmentNumber: 5,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Humor Editors",
+      members: [
+        {
+          picture: undefined,
+          name: `Michelle "Mike" Huang`,
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 6,
+        },
+        {
+          picture: undefined,
+          name: "Munem Tajwar",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 6,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Sports Editors",
+      members: [
+        {
+          picture: undefined,
+          name: "Elijah Choi",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 7,
+        },
+        {
+          picture: undefined,
+          name: "Leonardo Guidi",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 7,
+        },
+        {
+          picture: undefined,
+          name: "Kaileen So",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 7,
+        },
+        {
+          picture: undefined,
+          name: "Evan Wong",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 7,
+        },
+        {
+          picture: undefined,
+          name: "Duncan Park",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 7,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Photography Editors",
+      members: [
+        {
+          picture: undefined,
+          name: "Geoffry Huang",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 8,
+        },
+        {
+          picture: undefined,
+          name: "Ibtida Khurshed",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 8,
+        },
+        {
+          picture: undefined,
+          name: "Honora Muratori",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 8,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Art Directors",
+      members: [
+        {
+          picture: undefined,
+          name: "Stacey Chen",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 9,
+        },
+        {
+          picture: undefined,
+          name: "Jaden Bae",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 9,
+        },
+        {
+          picture: undefined,
+          name: "Rhea Malhotra",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 9,
+        },
+        {
+          picture: undefined,
+          name: "Chuer Zhong",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 9,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Layout Editors",
+      members: [
+        {
+          picture: undefined,
+          name: "Anjali Bechu",
+          managingBoard: false,
+          editorsInTraining: true,
+          departmentNumber: 10,
+        },
+        {
+          picture: undefined,
+          name: "Elaine Liu",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 10,
+        },
+        {
+          picture: undefined,
+          name: "Isabel Noh",
+          managingBoard: false,
+          editorsInTraining: true,
+          departmentNumber: 10,
+        },
+        {
+          picture: undefined,
+          name: "Andre Wang",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 10,
+        },
+        {
+          picture: undefined,
+          name: "Karen Xu",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 10,
+        },
+        {
+          picture: undefined,
+          name: "Jasper Yu-Dawidowicz",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 10,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Copy Editors",
+      members: [
+        {
+          picture: undefined,
+          name: "Yuna Lee",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 11,
+        },
+        {
+          picture: undefined,
+          name: "Ryan Park",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 11,
+        },
+        {
+          picture: undefined,
+          name: "Niamh Werner",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 11,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Buisness Managers",
+      members: [
+        {
+          picture: undefined,
+          name: "Fiona Cai",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 12,
+        },
+        {
+          picture: undefined,
+          name: "Christopher Louie",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 12,
+        },
+        {
+          picture: undefined,
+          name: "Anderson Oh",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 12,
+        },
+        {
+          picture: undefined,
+          name: "Everett Yu-Dawidowicz",
+          managingBoard: false,
+          editorsInTraining: true,
+          departmentNumber: 12,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Web Editors",
+      members: [
+        {
+          picture: undefined,
+          name: "Adam Choi",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 13,
+        },
+        {
+          picture: undefined,
+          name: "Angela Mashuryan",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 13,
+        },
+        {
+          picture: undefined,
+          name: "Lenny Metlitsky",
+          managingBoard: true,
+          editorsInTraining: false,
+          departmentNumber: 13,
+        },
+        {
+          picture: undefined,
+          name: "Ankita Saha",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 13,
+        },
+      ],
+    },
+    {
+      departmentTitle: "Faculty Advisor",
+      members: [
+        {
+          picture: undefined,
+          name: "Kerry Garfinkel",
+          managingBoard: false,
+          editorsInTraining: false,
+          departmentNumber: 14,
+        },
+      ],
+    },
+  ];
+  const [numberArray, setNumberArray] = useState<number[]>([]);
+  useEffect(() => {
+    const newArray = [];
+    for (let i = 0; i < 1000; i++) {
+      newArray.push(Math.floor(Math.random() * 13) + 1);
+    }
+    setNumberArray(newArray);
+  }, []);
+  return (
+    <>
+      <Head>{generateMetaTags(page_title, meta_description, meta_url)}</Head>
+      <main id={styles.main}>
+        <div className={styles.header}>
+          <div id={styles.text}>
+            <h1>Members of the Spectator Editorial Board</h1>
+          </div>
+          <div id={styles.tagScrollContainer}>
+              <div id={styles.tagScrollContent}>
+                {numberArray.map((value, _) => {
+                  return <DepartmentTags departmentNumber={value} />;
+                })}
+              </div>
+            </div>
+        </div>
+        <div id={styles.curve}>
+          <svg
+            id="visual"
+            viewBox="0 0 960 100"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+          >
+            <path
+              d="M0 52L32 49.2C64 46.3 128 40.7 192 39.3C256 38 320 41 384 44.5C448 48 512 52 576 51.7C640 51.3 704 46.7 768 43.7C832 40.7 896 39.3 928 38.7L960 38L960 0L928 0C896 0 832 0 768 0C704 0 640 0 576 0C512 0 448 0 384 0C320 0 256 0 192 0C128 0 64 0 32 0L0 0Z"
+              fill="#2b2b2b"
+              stroke-linecap="round"
+              stroke-linejoin="miter"
+            ></path>
+          </svg>
+        </div>
+        <section id={styles.departments}>
+          <p className={styles.special_def}>* Managing Board</p>
+          <p className={styles.special_def}>** Editors-in-Training</p>
+          {sectionalDepartment.map((value, index) => {
+            department = department + 1;
+            return (
+              <div key={index} className={styles.deprartmentContainer}>
+                <h1 id={styles.title}>{value.departmentTitle}</h1>
+                <div className={styles.cardsContainer}>
+                  {value.members.map((value, _) => {
+                    return (
+                      <Card
+                        picture={value.picture}
+                        name={value.name}
+                        departmentNumber={value.departmentNumber + 1}
+                        managingBoard={value.managingBoard}
+                        editorsInTraining={value.editorsInTraining}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </main>
+    </>
+  );
+}
 
 export default StaffPage;
